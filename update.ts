563914bdbe3c90a3c5b7b0d0e266d0121e567b40
Anand Chowdhary <mail@anandchowdhary.com>
2020-04-17 02:21:35 +0530

    :heavy_plus_sign: Add @types/node

--- update.ts
+++ update.ts
@@ -1,2 +1,15 @@
 import { config } from "dotenv";
 config();
+
+import { google } from "googleapis";
+const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_DOCS_CLIENT_ID,
+  process.env.GOOGLE_DOCS_CLIENT_SECRET,
+  "https://developers.google.com/oauthplayground"
+);
+oauth2Client.setCredentials({
+  access_token: process.env.GOOGLE_DOCS_ACCESS,
+  refresh_token: process.env.GOOGLE_DOCS_REFRESH,
+});
+
+const docs = google.docs("v1");
